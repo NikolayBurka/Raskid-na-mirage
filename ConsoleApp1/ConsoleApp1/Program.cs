26b95a6aba515c9d7e2a1780a75314133cecc733
Nikolay Burka <nikolay.burka@bk.ru>
2023-03-26 14:33:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Число Массива=");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = rnd.Next(1, 121);
+        }
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        int max = array[0];
+        int min = array[0];
+        int maxIndex = 0;
+        int minIndex = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+                maxIndex = i;
+            }
+            if (array[i] < min)
+            {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        Console.WriteLine("Максимальный элемент: " + max);
+        Console.WriteLine("Минимальный элемент: " + min);
+        int temp = array[maxIndex];
+        array[maxIndex] = array[minIndex];
+        array[minIndex] = temp;
+        Console.WriteLine("Измененный массив:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
